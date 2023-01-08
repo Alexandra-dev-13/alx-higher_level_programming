489ee88e59ca256809d3b58e6ad659fdf5014734
veketo <veketo@Ubuntu.myguest.virtualbox.org>
2023-01-08 02:27:07 +0000

    3. When I was having that alphabet soup

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -3,5 +3,4 @@
 list = "abcdefghijklmnopqrstuvwxyz"
 for letter in list:
     if letter != 'e' and letter != 'q':
-         print("{}".format(letter), end="")
-
+        print("{}".format(letter), end="")
